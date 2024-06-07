5827054bc30b1cd73ec62c5277bcacbf75dc4006
Roberto Ruiz <112975092+CopitoFrostbite@users.noreply.github.com>
2024-06-07 00:43:32 -0600

    e

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -10,34 +10,58 @@ const getUser = async (req, res) => {
   
   const createUser = async (req, res) => {
     const { username, name, lastname, email, password } = req.body;
+    
     try {
+      // Verificar que todos los datos requeridos están presentes
       if (!email || !password || !username || !name || !lastname) {
         return res.status(400).json({ message: 'Faltan datos requeridos' });
-    }
-      if (req.files?.File) {
-        const result = await uploadImage(req.files.File.tempFilePath);
-        const newUser = new User({
-          username: username,
-          name: name,
-          lastname: lastname,
-          email: email,
-          password: password,
-          avatar: {
-            public_id: result.public_id,
-            url_image: result.secure_url
-          }
-        });
-  
-        await newUser.save();
-  
-        res.status(201).json({ message: 'Usuario creado con éxito', user: newUser });
-      } else {
-        res.status(400).json({ message: 'No se subió ningún archivo' });
       }
+  
+      // Verificar si se subió un archivo
+      if (!req.files || !req.files.avatar) {
+        return res.status(400).json({ message: 'No se subió ningún archivo' });
+      }
+  
+      // Obtener el archivo
+      const avatar = req.files.avatar;
+  
+      // Subir la imagen (aquí debes reemplazar 'uploadImage' con tu propia lógica de subida)
+      const result = await uploadImage(avatar.tempFilePath);
+  
+      // Crear un nuevo usuario
+      const newUser = new User({
+        username: username,
+        name: name,
+        lastname: lastname,
+        email: email,
+        password: password,
+        avatar: {
+          public_id: result.public_id,
+          url_image: result.secure_url
+        }
+      });
+  
+      // Guardar el nuevo usuario en la base de datos
+      await newUser.save();
+
+      const transformedUser = {
+        userId: newUser._id,
+        username: newUser.username,
+        name: newUser.name,
+        lastname: newUser.lastname,
+        email: newUser.email,
+        password: newUser.password,
+        profilePicture: newUser.avatar.url_image
+      };
+      const response = { message: 'Usuario creado con éxito', user: transformedUser };
+      res.status(201).json(response);
+      console.log("Response Object:", response);
     } catch (error) {
+      console.error("Error al crear el usuario:", error);
       res.status(500).json({ message: 'Error al intentar crear el usuario', error: error });
+      
     }
-  }
+  };
 
   const getUserById = async (req, res) => {
     console.log('Function: getProductById');
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -5,7 +5,7 @@ const upload = require('../cloudinary/multer');
 
 // Definimos las rutas y conectamos con los controsladores
 router.get('/user', UserController.getUser);
-router.post('/user', UserController.createUser);
+router.post('/user/register', UserController.createUser);
 router.get('/user/:id', UserController.getUserById);
 
 module.exports = router;
