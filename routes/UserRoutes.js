fd856927f10456a7483445b7f31d8976598c1c47
Roberto <roberto_ppwp@hotmail.com>
2024-10-23 11:17:04 -0600

    a

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -5,7 +5,10 @@ const upload = require('../cloudinary/multer');
 
 // Definimos las rutas y conectamos con los controladores
 router.get('/user', UserController.getUser);
-router.post('/user/register', upload.single('avatar'), UserController.createUser);
+router.post('/user/register', upload.single('avatar'), (req, res, next) => {
+    console.log("Middleware de multer ejecutado, archivo recibido:", req.file);
+    next();
+  }, UserController.createUser);
 router.post('/user/login', UserController.loginUser);
 router.get('/user/:id', UserController.getUserById);
 
