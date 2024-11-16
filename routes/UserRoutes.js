e0ca4c271ae41b210a0b45cdb8a820028964dc14
Roberto Ruiz <112975092+CopitoFrostbite@users.noreply.github.com>
2024-11-16 16:50:52 -0600

    getuser comentado

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -4,7 +4,7 @@ const UserController = require('../controllers/UserController');
 const upload = require('../cloudinary/multer');
 
 // Definimos las rutas y conectamos con los controladores
-router.get('/user', UserController.getUser);
+//router.get('/user', UserController.getUser);
 router.post('/user/register', upload.single('avatar'), (req, res, next) => {
     console.log("Middleware de multer ejecutado, archivo recibido:", req.file);
     next();
