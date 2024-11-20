b926112ba2e5d92e8a05253d4fac676c4ad338ac
Roberto Ruiz <112975092+CopitoFrostbite@users.noreply.github.com>
2024-11-19 21:41:12 -0600

    error tipo dato

--- models/ImageModel.js
+++ models/ImageModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const imageSchema = new mongoose.Schema({
   imageId: { type: String, required: true, unique: true }, 
-  journalId: { type: mongoose.Schema.Types.ObjectId, ref: 'Journal', required: true },
+  journalId: { type: String, unique: true },
   filePath: { type: String, required: true }, 
   cloudUrl: { type: String, default: null }, 
   dateAdded: { type: Date, default: Date.now }, 
