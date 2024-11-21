2bed7c41e0ca9d28b74cef87530e4514b23f4f54
Roberto Ruiz <112975092+CopitoFrostbite@users.noreply.github.com>
2024-11-20 18:53:01 -0600

    aaa

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -68,7 +68,7 @@ const addImageToEntry = async (req, res) => {
     console.log("Received journalId:", journalId);
     try {
         // Buscar imágenes asociadas al journalId
-        const images = await Image.find({ journalId, isDeleted: false });
+        const images = await Image.find({ journalId: journalId, isDeleted: false });
 
         if (!images || images.length === 0) {
             return res.status(404).json({ message: 'No se encontraron imágenes para este journal' });
